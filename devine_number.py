c3c9dd87bc05504877a35e85e0ff293cd1671917
Ali <43767369aliou@gmail.com>
2025-08-31 23:05:58 +0100

    test jeuxdevine

---
+++ devine_number.py
@@ -0,0 +1,18 @@
+import random
+r = random.randint(1, 10)
+tantative = 3 
+print("Bienvenue dans le jeu du nombre devinette !")
+print("Vous avez 3 tentatives pour deviner le nombre entre 1 et 10.")
+while tantative > 0:
+    user = int(input("Entrez votre nombre : "))
+    if user == r:
+        print(f"Félicitations ! Vous avez deviné le bon nombre.{r}")
+        break
+    elif user < r:
+        print("C'est plus grand !")
+    else:
+        print("C'est plus petit !")
+    tantative -= 1
+    print(f"Il vous reste {tantative} tentatives.")
+if tantative == 0:
+    print(f"Vous avez épuisé vos tentatives. Le nombre était {r}.")
